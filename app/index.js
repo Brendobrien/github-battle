b2cb40d18235d84c1ccf1745cd01188c4576226c
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-07-12 11:54:43 +0000

    Tantra

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+    name: "Brendan O'Brien",
+    username: "Brendobrien",
+    image: "https://avatars2.githubusercontent.com/u/12202085?v=3&u=2656c66e79af91400345e7e372634823c17cd3a8&s=140"
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function() {
+var ProfilePic = React.createClass({
+    render: function(){
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    }
+});
+
+var ProfileLink = React.createClass({
+  render: function(){
+      return (
+          <div>
+            <a href={'https://github.com/' + this.props.username}>
+                {this.props.username}
+            </a>
+          </div>
+      )
+  }  
+})
+
+var ProfileName = React.createClass({
+    render: function(){
         return (
-            <div> Hello World </div>
+            <div>{this.props.name}</div>
         )
     }
 });
 
+var Avatar = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image}/>
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+})
+
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />, 
     document.getElementById('app')
 );
