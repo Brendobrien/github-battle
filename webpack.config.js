f0455b3bdd35ae24340ebc8f85d0a708902496ff
Brendan O'Brien <obrienpbrendan@gmail.com>
2016-07-12 10:40:27 +0000

    Cloud 9 makes things tough

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?https://0.0.0.0:8080',
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
